c9ce5f309a65d7e5879a7a989e14bdd12ac1cb30
Zach Gee <zach.gee28@gmail.com>
2021-05-23 09:35:56 -0700

    Removed port settings on app.js to be deployable on heroku

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.engine('handlebars', handlebars({
   }
 }))
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+// app.set('port', 3000);
 app.use(express.static('public'));
 
 app.get('/',function(req,res){
@@ -47,6 +47,6 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-});
+// app.listen(app.get('port'), function(){
+//   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+// });
