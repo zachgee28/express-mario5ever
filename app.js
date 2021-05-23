44356b3c1db47ecff037fcaea88fb3a5a8c9ecb4
Zach Gee <zach.gee28@gmail.com>
2021-05-23 11:11:40 -0700

    troubleshooting app deployment

--- app.js
+++ app.js
@@ -50,3 +50,5 @@ app.use(function(err, req, res, next){
 // app.listen(app.get('port'), function(){
 //   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 // });
+
+module.exports = app;//
